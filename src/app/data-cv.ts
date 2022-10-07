0eb7c9d023d74f2a21152fca25bb03dbbda48cbc
Jmongrio <jmongrio@hotmail.com>
2022-10-07 15:14:12 -0600

    CV update

---
+++ src/app/data-cv.ts
@@ -0,0 +1,10 @@
+export interface DataCV {
+    name: string;
+    job: string;
+    phone: string;
+    email: string;
+    address: string;
+    linkedi: string;
+    github: string;
+    description: string
+}
---
+++ src/app/education.ts
@@ -0,0 +1,5 @@
+export interface Education {
+    name: string;
+    university: string;
+    year: Int16Array;
+}
